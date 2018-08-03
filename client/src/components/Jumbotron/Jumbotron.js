2cc6d711e6e4bc4e5bd98a03437ccbf7337630b6
Dan Fabry <dfabry14@gmail.com>
2018-08-03 14:23:16 -0500

    changed some styling

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -4,8 +4,7 @@ import "./jumbotron.css";
 const Jumbotron = () => (
     <div className="size jumbotron jumbotron-fluid">
         <div className="container">
-            <h1 className="header">Trivia Time!</h1><p>github logo</p>
-            <p className="inst">Click play to start!</p>
+            <h1 className="header">Trivia Time!</h1>
             {/* <p className="inst">Invite your friends for some competition!</p> */}
         </div>
     </div>
